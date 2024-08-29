bf96cbd455b909dc2f3ca1b158f75aafb5f6239b
Ashton A <uhdbits@duck.com>
2024-08-28 21:21:40 -0700

    Update packages
    This commit simply just updates the packages, and tests other FRC packages.

--- home/uhdbits/features/development/frc.nix
+++ home/uhdbits/features/development/frc.nix
@@ -1,5 +1,5 @@
 # Nix configuration file to install FRC related packages.
 { pkgs, inputs, ... }:
 {
-  home.packages = [ inputs.frc-nix.packages.x86_64-linux.choreo ];
+  home.packages = [ inputs.frc-nix.packages.x86_64-linux.elastic-dashboard ];
 }
