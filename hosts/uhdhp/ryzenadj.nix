9c0d0da3ae86c81e1e36e5eb7524ef4902353198
Ashton A. <uhdbits@duck.com>
2025-07-07 20:52:03 -0700

    Update ryzenadj configuration
    Various updates to the ryzenadj configuration, mainly boosting performance on battery. May adjust a bit later on if battery life is too low.
    
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- hosts/uhdhp/ryzenadj.nix
+++ hosts/uhdhp/ryzenadj.nix
@@ -2,24 +2,25 @@
 { pkgs, ... }:
 let
   # These values will be set if the computer is on AC power (plugged in).
-  stapm-limit-ac = 30000;
-  fast-limit-ac = 30000;
-  slow-limit-ac = 25000;
+  stapm-limit-ac = 30001;
+  fast-limit-ac = 30001;
+  slow-limit-ac = 25001;
   slow-time-ac = 30;
-  stapm-time-ac = 0;
-  apu-skin-temp-ac = 80;
+  stapm-time-ac = 1;
+  apu-skin-temp-ac = 95;
 
   # These values will be set if the computer is on battery power (unplugged).
-  stapm-limit-bat = 10000;
-  fast-limit-bat = 15000;
-  slow-limit-bat = 10000;
-  slow-time-bat = 15;
-  stapm-time-bat = 0;
-  apu-skin-temp-bat = 40;
+  stapm-limit-bat = 15000;
+  fast-limit-bat = 20000;
+  slow-limit-bat = 15000;
+  slow-time-bat = 5;
+  stapm-time-bat = 1;
+  apu-skin-temp-bat = 45;
 
   # Create commands to run on AC and battery.
-  ryzenadj-ac-command = "${pkgs.ryzenadj}/bin/ryzenadj --stapm-limit=${toString stapm-limit-ac} --fast-limit=${toString fast-limit-ac} --slow-limit=${toString slow-limit-ac} --slow-time=${toString slow-time-ac} --stapm-time=${toString stapm-time-ac} --apu-skin-temp=${toString apu-skin-temp-ac} --max-performance";
-  ryzenadj-bat-command = "${pkgs.ryzenadj}/bin/ryzenadj --stapm-limit=${toString stapm-limit-bat} --fast-limit=${toString fast-limit-bat} --slow-limit=${toString slow-limit-bat} --slow-time=${toString slow-time-bat} --stapm-time=${toString stapm-time-bat} --apu-skin-temp=${toString apu-skin-temp-bat} --power-saving";
+  # We run ryzenadj twice, one to set the power-saving/max-performance, and again to set our custom settings, to prevent power-saving/max-performance from overwriting any of our settings (mainly slow-time).
+  ryzenadj-ac-command = "${pkgs.ryzenadj}/bin/ryzenadj --max-performance --stapm-limit=${toString stapm-limit-ac} --fast-limit=${toString fast-limit-ac} --slow-limit=${toString slow-limit-ac} --slow-time=${toString slow-time-ac} --stapm-time=${toString stapm-time-ac} --apu-skin-temp=${toString apu-skin-temp-ac}";
+  ryzenadj-bat-command = "${pkgs.ryzenadj}/bin/ryzenadj --power-saving --stapm-limit=${toString stapm-limit-bat} --fast-limit=${toString fast-limit-bat} --slow-limit=${toString slow-limit-bat} --slow-time=${toString slow-time-bat} --stapm-time=${toString stapm-time-bat} --apu-skin-temp=${toString apu-skin-temp-bat}";
 
   # Create udev rules to run when switching between AC power or battery power.
   ryzenadj-udev = pkgs.writeTextFile {
@@ -32,6 +33,9 @@ let
   };
 in
 {
+  # Enable "ryzen-smc" to prevent accessing /dev/mem.
+  hardware.cpu.amd.ryzen-smu.enable = true;
+
   # Add "ryzenadj" to the system packages list.
   environment.systemPackages = [ pkgs.ryzenadj ];
 
