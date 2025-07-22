317434c3ef0ad540aa73455d2f67f5aeab8fcf94
Ashton A. <uhdbits@duck.com>
2025-07-22 11:16:38 -0700

    Work on GNOME settings
    Work on adding my preferred GNOME settings.
    
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- home/uhdbits/features/desktop/gnome/settings.nix
+++ home/uhdbits/features/desktop/gnome/settings.nix
@@ -22,6 +22,34 @@ with lib.hm.gvariant;
     # Desktop interface configuration
     "org/gnome/desktop/interface" = {
       clock-format = "12h";
+      color-scheme = "prefer-dark";
+      show-battery-percentage = true;
+    };
+
+    "org/gnome/desktop/session" = {
+      idle-delay = 600;
+    }
+
+    "org/gnome/desktop/sound" = {
+      event-sounds = false;
+    };
+
+    # Night light configuration
+    "org/gnome/settings-daemon/plugins/color" = {
+      night-light-enabled = true;
+      night-light-schedule-automatic = false;
+      night-light-schedule-from = 20;
+      night-light-schedule-to = 6;
+      night-light-temperature = 2700;
+    };
+
+    "org/gnome/settings-daemon/plugins/power" = {
+      idle-dim = true;
+      power-button-action = "suspend";
+      sleep-inactive-battery-type = "suspend";
+      sleep-inactive-battery-timeout = 900;
+      sleep-inactive-ac-type = "suspend";
+      sleep-inactive-ac-timeout = 1200;
     };
 
     # Geolocation settings (just disable)
