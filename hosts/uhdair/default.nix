afe6840c43779a041ef3128fbb58d97b27d84ac7
Ashton A. <uhdbits@duck.com>
2026-01-03 07:51:59 -0700

    Add GDM monitors.xml transfer
    Just found online somewhere and only temporary for now, hope to have a better solution later.
    
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- hosts/uhdair/default.nix
+++ hosts/uhdair/default.nix
@@ -60,6 +60,26 @@
 
   system.stateVersion = "25.05";
 
+  systemd.services.applyUserMonitorSettings =
+    let
+      username = "uhdbits";
+      gdmConfigDir = "/var/lib/gdm/seat0/config";
+    in
+    {
+      description = "Apply user monitor settings to GDM login screen";
+      after = [
+        "network.target"
+        "systemd-user-sessions.service"
+        "display-manager.service"
+      ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.bash}/bin/bash -c 'echo \"Applying user monitor settings to GDM login screen\" && mkdir -p ${gdmConfigDir} && echo \"Created ${gdmConfigDir} directory\" && [ \"/home/${username}/.config/monitors.xml\" -ef \"${gdmConfigDir}/monitors.xml\" ] || cp /home/${username}/.config/monitors.xml ${gdmConfigDir}/monitors.xml && echo \"Copied monitors.xml to ${gdmConfigDir}/monitors.xml\" && chown gdm:gdm ${gdmConfigDir}/monitors.xml && echo \"Changed ownership of monitors.xml to gdm\"'";
+      };
+    };
+
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
