76855fe8ccc0c732188eb2c5899d704be7dc785c
Ashton A. <uhdbits@duck.com>
2026-02-10 20:27:53 -0700

    Continue working on inputs
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- flake.nix
+++ flake.nix
@@ -2,17 +2,23 @@
 {
   # Inputs, the dependencies (online or local) of this flake.
   inputs = {
+    frc-nix.url = {
+      url = "github:frc4451/frc-nix/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    impermanence.url = "github:nix-community/impermanence";
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v1.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 }
