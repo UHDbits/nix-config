5c1e4e5c284cf628aceb485b76110d36390b51c4
Ashton A. <uhdbits@duck.com>
2026-09-13 18:45:47 -0700

    feat: Start rewrite, again
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- modules/nixos/base/misc.nix
+++
@@ -1,6 +0,0 @@
-# NixOS configuration for generic base configurations.
-{
-  hardware.usbStorage.manageShutdown = true;
-  services.fwupd.enable = true;
-  services.upower.enable = true;
-}
--- modules/nixos/base/overlays.nix
+++
@@ -1,5 +0,0 @@
-{ inputs, ... }:
-{
-  nixpkgs.overlays = [ inputs.frc-nix.overlays.default
-    inputs.nix-cachyos-kernel.overlays.default ];
-}
