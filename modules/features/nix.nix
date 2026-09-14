1569956f46e6ed0c178cd6a1879d5f40c9be4762
Ashton A. <uhdbits@duck.com>
2026-09-13 20:33:44 -0700

    feat: add nix.nix
    Signed-off-by: Ashton A. <uhdbits@duck.com>

---
+++ modules/features/nix.nix
@@ -0,0 +1,40 @@
+{ inputs, ... }:
+{
+  imports = [ inputs.nix-index-database.nixosModules.default ];
+
+  nix = {
+    channel.enable = false;
+    nixPath = [ "nixpkgs=flake:nixpkgs" ];
+    registry.nixpkgs.flake = inputs.nixpkgs;
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "flakes"
+        "nix-command"
+      ];
+      flake-registry = "";
+      substituters = [
+        "https://attic.xuyh0120.win/lantian"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      trusted-users = [
+        "root"
+        "uhdbits"
+      ];
+    };
+  };
+
+  nixpkgs = {
+    config.allowUnfree = true;
+    overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ];
+  };
+
+  programs = {
+    nix-index.enable = true;
+    nix-index-database.enable = true;
+  };
+}
