391eba00a0db7accb6078a349fe0267accae9273
Ashton A. <uhdbits@duck.com>
2026-09-25 20:37:08 -0700

    feat: Add additional apps to niri desktop

--- modules/features/desktops/niri.nix
+++ modules/features/desktops/niri.nix
@@ -1,10 +1,16 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 {
   config = lib.mkIf (config.desktop == "niri") {
+    environment.systemPackages = with pkgs; [
+      ghostty
+      nautilus
+    ];
+
     programs.niri.enable = true;
     services.displayManager.gdm.enable = true;
   };
